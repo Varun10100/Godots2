78c06a206d8a78b03f053df58cd5bf7a1f8446d0
Varun10100 <166926913+Varun10100@users.noreply.github.com>
2024-04-14 22:36:24 +0530

    13

--- Assets/Move_boy.gd
+++ Assets/Move_boy.gd
@@ -2,6 +2,7 @@ extends CharacterBody3D
 
 var Velocity  = Vector3(0,0,0)
 const SPEED = 5
+const  RotateSpeed = 10
 
 func _ready():
 	pass
@@ -10,15 +11,19 @@ func _physics_process(delta):
 		velocity.z = 0
 	elif Input.is_action_pressed("ui_right"):
 		velocity.z = -SPEED
+		$MeshInstance3Ds.rotate_x(deg_to_rad(-RotateSpeed))
 	elif Input.is_action_pressed("ui_left"):
+		$MeshInstance3Ds.rotate_x(deg_to_rad(RotateSpeed))
 		velocity.z = SPEED
 	else:
 		velocity.z = lerp(velocity.z,0.0,0.1)
 	if Input.is_action_pressed("ui_up") and Input.is_action_pressed("ui_down"):
 		velocity.x = 0
 	elif Input.is_action_pressed("ui_up"):
+		$MeshInstance3Ds.rotate_z(deg_to_rad(RotateSpeed))
 		velocity.x = -SPEED
 	elif Input.is_action_pressed("ui_down"):
+		$MeshInstance3Ds.rotate_z(deg_to_rad(-RotateSpeed))
 		velocity.x = SPEED
 	else:
 		velocity.x = lerp(velocity.x,0.0,0.1)
